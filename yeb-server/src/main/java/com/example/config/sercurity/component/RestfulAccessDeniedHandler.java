8a2f5cf24a4715bf862dc82aa05ef138f6cf58f7
mlNothing <utmostfires@gmail.com>
2021-11-16 12:44:09 +0800

    security

--- yeb-server/src/main/java/com/example/config/sercurity/component/RestfulAccessDeniedHandler.java
+++ yeb-server/src/main/java/com/example/config/sercurity/component/RestfulAccessDeniedHandler.java
@@ -1,5 +1,32 @@
-package com.example.config.sercurity.component;/**
-*@author mlNothing
-*@date 2021/11/16 12:32
-*/    public class RestfulAccessDeniedHandler {
+package com.example.config.sercurity.component;
+
+import com.example.pojo.RespBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author mlNothing
+ * @date 2021/11/16 12:32
+ */
+@Component
+public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        RespBean bean = RespBean.error("权限不足，请联系管理员！");
+        bean.setCode(403);
+        out.write(new ObjectMapper().writeValueAsString(bean));
+        out.flush();
+        out.close();
+    }
 }
