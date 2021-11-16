fdba7444528daac73cc5de6624a38136f107552f
mlNothing <utmostfires@gmail.com>
2021-11-16 12:43:36 +0800

    security

--- yeb-server/src/main/java/com/example/config/sercurity/component/RestAuthenticationEntryPoint.java
+++ yeb-server/src/main/java/com/example/config/sercurity/component/RestAuthenticationEntryPoint.java
@@ -1,5 +1,32 @@
-package com.example.config.sercurity.component;/**
-*@author mlNothing
-*@date 2021/11/16 12:30
-*/    public class RestAuthenticationEntryPoint {
+package com.example.config.sercurity.component;
+
+import com.example.pojo.RespBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author mlNothing
+ * @date 2021/11/16 12:30
+ */
+@Component
+public class RestAuthenticationEntryPoint  implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        RespBean bean = RespBean.error("权限不足，请联系管理员！");
+        bean.setCode(401);
+        out.write(new ObjectMapper().writeValueAsString(bean));
+        out.flush();
+        out.close();
+    }
 }
