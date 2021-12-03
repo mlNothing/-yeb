7a7338c8393466d1baf962ca7fc1b2c8e52c89e5
mlNothing <utmostfires@gmail.com>
2021-12-03 09:33:53 +0800

    添加了注释

--- yeb-server/src/main/java/com/example/pojo/AdminLoginParam.java
+++ yeb-server/src/main/java/com/example/pojo/AdminLoginParam.java
@@ -21,5 +21,5 @@ public class AdminLoginParam {
     @ApiModelProperty(value = "密码", required = true)
     private String password;
     @ApiModelProperty(value = "验证码" ,required = true)
-    private  String Code;
+    private  String code;
 }
