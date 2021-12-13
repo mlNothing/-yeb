71d4bc31e68ea57718d72d6ed40b3826dd109dc1
mlNothing <utmostfires@gmail.com>
2021-12-13 16:20:30 +0800

    工资账套的增删改查

--- yeb-server/src/main/java/com/example/controller/SalaryController.java
+++ yeb-server/src/main/java/com/example/controller/SalaryController.java
@@ -1,8 +1,15 @@
 package com.example.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.pojo.RespBean;
+import com.example.pojo.Salary;
+import com.example.service.ISalaryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +20,61 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-11-16
  */
 @RestController
-@RequestMapping("/salary")
+@RequestMapping("/salary/sob")
 public class SalaryController {
+    @Autowired
+    private ISalaryService salaryService;
+
+    /**
+     * create by: mlNothing
+     * description: 获取
+     * create time: 2021/12/12 23:50
+     * @param
+     * @return list
+     */
+    @ApiOperation("获取所有的工资账套")
+    @GetMapping("/")
+    public List<Salary> getAllSalaries(){
+        return salaryService.list();
+    }
+
+    /**
+     * create by: mlNothing
+     * description: 更新工资账套
+     * create time: 2021/12/12 23:50
+     * @param salary salary
+     * @return
+     */
+    @ApiOperation("添加工资账套")
+    @PostMapping("/")
+    public RespBean addSalary(@RequestBody Salary salary){
+        salary.setCreateDate(LocalDateTime.now());
+        if (salaryService.save(salary)){
+            return RespBean.success("添加成功");
+        }return RespBean.error("添加失败");
+    }
+
+    /**
+     * create by: mlNothing
+     * description:
+     * create time: 2021/12/12 23:53
+     * @param id id
+     * @return
+     */
+    @ApiOperation("删除工资账套")
+    @DeleteMapping("/{id}")
+    public RespBean deleteSalaryById(@PathVariable Integer id){
+        if (salaryService.removeById(id)){
+            return RespBean.success("删除成功");
+        }return RespBean.error("删除失败");
+    }
+
+    @ApiOperation("更新工资账套")
+    @PutMapping("/")
+    public RespBean updateSalary(@RequestBody Salary salary){
+        if (salaryService.updateById(salary)){
+            return RespBean.success("更新成功");
+        }return RespBean.error("更新失败");
+    }
 
 }
