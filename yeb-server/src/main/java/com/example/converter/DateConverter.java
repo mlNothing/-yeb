3e3436ef129faf36cc5c195769c56fb6bae6c73d
mlNothing <utmostfires@gmail.com>
2022-01-21 13:58:30 +0800

    更新返回小问题

--- yeb-server/src/main/java/com/example/converter/DateConverter.java
+++ yeb-server/src/main/java/com/example/converter/DateConverter.java
@@ -1,7 +1,10 @@
 package com.example.converter;
 
 
-import org.springframework.core.convert.converter.Converter;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -16,12 +19,22 @@ public class DateConverter implements Converter<String, LocalDate> {
 
 
     @Override
-    public LocalDate convert(String source) {
+    public LocalDate convert(String s) {
         try {
-            return LocalDate.parse(source, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            return LocalDate.parse(s, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    @Override
+    public JavaType getInputType(TypeFactory typeFactory) {
+        return null;
+    }
+
+    @Override
+    public JavaType getOutputType(TypeFactory typeFactory) {
         return null;
     }
 }
